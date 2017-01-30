592f39e7b65fc1c0e795e80c0b79717d926a27fa
AldousP <aldouspryce@gmail.com>
2017-01-30 11:21:43 -0500

    Convert to Java project.

---
+++ src/MainApp.java
@@ -0,0 +1,8 @@
+import processing.core.PApplet;
+
+public class MainApp extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("BlankSlate", args);
+    }
+}
---
+++ src/ProcessingApp.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+/**
+ * Common methods used across sketches.
+ */
+public class ProcessingApp extends PApplet {
+
+
+}
