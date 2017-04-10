420a08d4fa433a02da0378a64fbc913af69c43cc
AldousP <aldouspryce@gmail.com>
2017-04-10 12:52:39 -0400

    Switch to grid pane.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -21,15 +27,17 @@ public class HelloWorld extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
 
-        Scene scene = new Scene(root, WIDTH, HEIGHT);
-
+        Scene scene = new Scene(grid, WIDTH, HEIGHT);
         primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
